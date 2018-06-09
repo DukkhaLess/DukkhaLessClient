f689405df7708d3286f426a3a4a6caf75b761ad3
Alexandra DeWit <alexa.dewit@gmail.com>
2018-06-09 17:27:48 +0200

    Type defs and camelCase naming

--- src/Style/Bulogen/Aria/Role.purs
+++ src/Style/Bulogen/Aria/Role.purs
@@ -1,73 +1,187 @@
 module Style.Bulogen.Aria.Role where
 
-import Halogen.HTML (AttrName(..))
 import Halogen.HTML.Properties as HP
-import Style.Bulogen.Properties as BP
 import Style.Bulogen.Properties (role)
 
+alert :: forall r i. HP.IProp r i
 alert = role "alert"
-alertdialog = role "alertdialog"
+
+alertDialog :: forall r i. HP.IProp r i
+alertDialog = role "alertdialog"
+
+button :: forall r i. HP.IProp r i
 button = role "button"
+
+checkbox :: forall r i. HP.IProp r i
 checkbox = role "checkbox"
+
+dialog :: forall r i. HP.IProp r i
 dialog = role "dialog"
-gridcell = role "gridcell"
+
+gridCell :: forall r i. HP.IProp r i
+gridCell = role "gridcell"
+
+link :: forall r i. HP.IProp r i
 link = role "link"
+
+log :: forall r i. HP.IProp r i
 log = role "log"
+
+marquee :: forall r i. HP.IProp r i
 marquee = role "marquee"
-menuitem = role "menuitem"
-menuitemcheckbox = role "menuitemcheckbox"
-menuitemradio = role "menuitemradio"
+
+menuItem :: forall r i. HP.IProp r i
+menuItem = role "menuitem"
+
+menuItemCheckbox :: forall r i. HP.IProp r i
+menuItemCheckbox = role "menuitemcheckbox"
+
+menuItemRadio :: forall r i. HP.IProp r i
+menuItemRadio = role "menuitemradio"
+
+option :: forall r i. HP.IProp r i
 option = role "option"
-progressbar = role "progressbar"
+
+progressBar :: forall r i. HP.IProp r i
+progressBar = role "progressbar"
+
+radio :: forall r i. HP.IProp r i
 radio = role "radio"
+
+scrollbar :: forall r i. HP.IProp r i
 scrollbar = role "scrollbar"
+
+slider :: forall r i. HP.IProp r i
 slider = role "slider"
-spinbutton = role "spinbutton"
+
+spinButton :: forall r i. HP.IProp r i
+spinButton = role "spinbutton"
+
+status :: forall r i. HP.IProp r i
 status = role "status"
+
+tab :: forall r i. HP.IProp r i
 tab = role "tab"
-tabpanel = role "tabpanel"
+
+tabPanel :: forall r i. HP.IProp r i
+tabPanel = role "tabpanel"
+
+textbox :: forall r i. HP.IProp r i
 textbox = role "textbox"
+
+timer :: forall r i. HP.IProp r i
 timer = role "timer"
+
+tooltip :: forall r i. HP.IProp r i
 tooltip = role "tooltip"
-treeitem = role "treeitem"
+
+treeItem :: forall r i. HP.IProp r i
+treeItem = role "treeitem"
 
 
-combobox = role "combobox"
+comboBox :: forall r i. HP.IProp r i
+comboBox = role "combobox"
+
+grid :: forall r i. HP.IProp r i
 grid = role "grid"
-listbox = role "listbox"
+
+listBox :: forall r i. HP.IProp r i
+listBox = role "listbox"
+
+menu :: forall r i. HP.IProp r i
 menu = role "menu"
-menubar = role "menubar"
-radiogroup = role "radiogroup"
-tablist = role "tablist"
+
+menuBar :: forall r i. HP.IProp r i
+menuBar = role "menubar"
+
+radioGroup :: forall r i. HP.IProp r i
+radioGroup = role "radiogroup"
+
+tabList :: forall r i. HP.IProp r i
+tabList = role "tablist"
+
+tree :: forall r i. HP.IProp r i
 tree = role "tree"
-treegrid = role "treegrid"
+
+treeGrid :: forall r i. HP.IProp r i
+treeGrid = role "treegrid"
 
 
+article :: forall r i. HP.IProp r i
 article = role "article"
-columnheader = role "columnheader"
+
+columnHeader :: forall r i. HP.IProp r i
+columnHeader = role "columnheader"
+
+definition :: forall r i. HP.IProp r i
 definition = role "definition"
+
+directory :: forall r i. HP.IProp r i
 directory = role "directory"
+
+document :: forall r i. HP.IProp r i
 document = role "document"
+
+group :: forall r i. HP.IProp r i
 group = role "group"
+
+heading :: forall r i. HP.IProp r i
 heading = role "heading"
+
+img :: forall r i. HP.IProp r i
 img = role "img"
+
+list :: forall r i. HP.IProp r i
 list = role "list"
-listitem = role "listitem"
+
+listItem :: forall r i. HP.IProp r i
+listItem = role "listitem"
+
+math :: forall r i. HP.IProp r i
 math = role "math"
+
+note :: forall r i. HP.IProp r i
 note = role "note"
+
+presentation :: forall r i. HP.IProp r i
 presentation = role "presentation"
+
+region :: forall r i. HP.IProp r i
 region = role "region"
+
+row :: forall r i. HP.IProp r i
 row = role "row"
-rowheader = role "rowheader"
+
+rowHeader :: forall r i. HP.IProp r i
+rowHeader = role "rowheader"
+
+separator :: forall r i. HP.IProp r i
 separator = role "separator"
+
+toolbar :: forall r i. HP.IProp r i
 toolbar = role "toolbar"
 
 
+application :: forall r i. HP.IProp r i
 application = role "application"
+
+banner :: forall r i. HP.IProp r i
 banner = role "banner"
+
+complementary :: forall r i. HP.IProp r i
 complementary = role "complementary"
-contentinfo = role "contentinfo"
+
+contentInfo :: forall r i. HP.IProp r i
+contentInfo = role "contentinfo"
+
+form :: forall r i. HP.IProp r i
 form = role "form"
+
+main :: forall r i. HP.IProp r i
 main = role "main"
+
+navigation :: forall r i. HP.IProp r i
 navigation = role "navigation"
+
+search :: forall r i. HP.IProp r i
 search = role "search"
