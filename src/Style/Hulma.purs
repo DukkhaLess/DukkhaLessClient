3781f5b045fe006a72d4daa46cbe1aab22432768
Alexandra DeWit <alexa.dewit@gmail.com>
2018-06-09 17:32:39 +0200

    Correctly attribute authorship for Bulogen code to James

--- src/Style/Hulma.purs
+++ src/Style/Hulma.purs
@@ -1,8 +1,22 @@
+{-
+Copyright 2018 James Laver
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-}
+
 module Style.Hulma where
 
-import Prelude
 import Halogen.HTML as HH
-import Halogen.HTML.Elements as HE
 import Halogen.HTML.Properties as HP
 
 -- card :: forall p i. Array ClassName -> Array (HTML p i) -> HTML p i
