7e891797035a99d7f37fb52774e1e2395823ebcb
Alexandra DeWit <alexa.dewit@gmail.com>
2018-06-09 17:03:52 +0200

    Further type definitions

--- src/Style/Bulogen/Properties.purs
+++ src/Style/Bulogen/Properties.purs
@@ -3,11 +3,15 @@ module Style.Bulogen.Properties where
 import Halogen.HTML (AttrName(..))
 import Halogen.HTML.Properties as HP
 
+attr :: forall r i. String -> String -> HP.IProp r i
 attr name = HP.attr (AttrName name)
 
+role :: forall r i. String -> HP.IProp r i
 role = attr "role"
 
+src :: forall r i. String -> HP.IProp r i
 src = attr "src"
 
+ariaLabel :: forall r i. String -> HP.IProp r i
 ariaLabel = attr "aria-label"
 
