fc2d2bc27cdab6c21be48048c87f420b22f03b3d
Alexandra DeWit <alexa.dewit@gmail.com>
2018-06-10 16:38:25 +0200

    Light improvement to string localiser

--- src/Intl.purs
+++ src/Intl.purs
@@ -2,19 +2,17 @@ module Intl
   (localiseString
   ) where
 
-import Prelude       (map)
-import Control.Alt   ((<|>))
-import Data.Array    (foldr)
-import Data.Maybe    (Maybe(..), fromMaybe)
-import Intl.Terms    (Term)
-import Intl.Locales  (Language(..))
-import Intl.English  (localiseEnglishString)
+import Data.Array (catMaybes, head)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Intl.English (localiseEnglishString)
+import Intl.Locales (Language(..))
+import Intl.Terms (Term)
+import Prelude (map, ($))
 
 localiseString :: Array Language -> Term -> String
-localiseString languages term = fromMaybe "Could not translate term." bestTerm where
+localiseString languages term = fromMaybe ">> Translation Missing <<" bestTerm where
   maybeTerms = map toTerm languages
-  bestTerm :: Maybe String
-  bestTerm = foldr (<|>) Nothing maybeTerms
+  bestTerm = head $ catMaybes $ maybeTerms
 
   -- | Language function mappings
   toTerm :: Language -> Maybe String
