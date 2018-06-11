0dd9afc80460c0aadd12c40467509954987b8d44
Alexandra DeWit <alexa.dewit@gmail.com>
2018-06-11 14:22:41 +0200

    Turn exposed versions into Aff

--- src/WebCrypto/Subtle.purs
+++ src/WebCrypto/Subtle.purs
@@ -1,8 +1,9 @@
 module WebCrypto.Subtle where
 
-import Control.Promise (toAffE, Promise)
+import Prelude         (($))
+import Effect.Aff      (Aff)
+import Control.Promise (toAff, Promise)
 import Data.Function.Uncurried (Fn3, runFn3)
-import Effect (Effect)
 
 foreign import data Algorithm :: Type
 foreign import data CryptoKey :: Type
@@ -12,8 +13,8 @@ foreign import data ArrayBuffer :: Type
 foreign import encryptImpl :: Fn3 Algorithm CryptoKey BufferSource (Promise ArrayBuffer)
 foreign import decryptImpl :: Fn3 Algorithm CryptoKey BufferSource (Promise String)
 
-encrypt :: Algorithm -> CryptoKey -> BufferSource -> Promise ArrayBuffer
-encrypt = runFn3 encryptImpl
+encrypt :: Algorithm -> CryptoKey -> BufferSource -> Aff ArrayBuffer
+encrypt alg key source = toAff $ runFn3 encryptImpl alg key source
 
-decrypt :: Algorithm -> CryptoKey -> BufferSource -> Promise String
-decrypt = runFn3 decryptImpl
+decrypt :: Algorithm -> CryptoKey -> BufferSource -> Aff String
+decrypt alg key source = toAff $ runFn3 decryptImpl alg key source
