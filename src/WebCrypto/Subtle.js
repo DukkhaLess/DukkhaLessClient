f79a705f13b8100c4637850830d013a4432744e6
Alexandra DeWit <alexa.dewit@gmail.com>
2018-07-04 17:59:06 +0200

    Remove webcrypto api work, in favour of tweetnacl

--- src/WebCrypto/Subtle.js
+++
@@ -1,7 +0,0 @@
-exports.encryptImpl = function(algorithm, key, data) {
-    return crypto.subtle.encrypt(algorithm, key, data);
-};
-
-exports.decryptImpl = function(algorithm, key, data) {
-    return crypto.subtle.decrypt(algorithm, key, data);
-};
