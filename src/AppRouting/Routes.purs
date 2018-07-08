0120780815c845e1d247c5a087dad3312bea76c2
Alexandra DeWit <alexa.dewit@gmail.com>
2018-07-08 16:21:40 +0200

    Create basic matchers

--- src/AppRouting/Routes.purs
+++ src/AppRouting/Routes.purs
@@ -1,5 +1,17 @@
 module AppRouting.Routes where
 
+import Prelude
+import Control.Alternative ((<|>))
+import Data.Foldable (oneOf)
+import Routing.Match (Match, lit, int, str, end)
+
 data Routes
   = Intro
   | Resources
+
+
+routes :: Match Routes
+routes = oneOf
+  [ Intro <$ lit "intro"
+  , Resources <$ lit "resources"
+  ]
