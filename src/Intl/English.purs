95e34ae39992dd266dcca76899747aff2eeb7f54
Alexandra DeWit <alexa.dewit@gmail.com>
2018-07-08 16:08:44 +0200

    Cleanup and add resources

--- src/Intl/English.purs
+++ src/Intl/English.purs
@@ -9,8 +9,11 @@ import Data.Maybe (Maybe(..))
 localise :: Term.Term -> Maybe String
 localise Term.MySelfCare = Just "My Selfcare"
 localise (Term.Intro intro) = localiseIntro intro
-localise _    = Nothing
+localise (Term.Resource resource) = localiseResource resource
 
 localiseIntro :: Intro.Introduction -> Maybe String
 localiseIntro Intro.Title = Just "My Selfcare"
 localiseIntro Intro.Explanation = Just "A Selfcare application with encryption to help work on your mental health"
+
+localiseResource :: Resource.Resources -> Maybe String
+localiseResource Resource.Title = Just "Resources"
