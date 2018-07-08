4058bc818bc9a1f38cf399ac17ed6a22c14a802c
Alexandra DeWit <alexa.dewit@gmail.com>
2018-07-08 16:14:12 +0200

    Correct file naming

--- src/AppRouting/AppRouter.purs
+++
@@ -1,2 +0,0 @@
-module AppRouting.Router where
-
---
+++ src/AppRouting/Router.purs
@@ -0,0 +1,10 @@
+module AppRouting.Router where
+
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import AppRouting.Routes
+
+
+
