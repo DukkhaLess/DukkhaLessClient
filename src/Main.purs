118b4556550d86397bf311b89a726f0c9438f29b
Alexandra DeWit <alexa.dewit@gmail.com>
2018-07-14 14:46:32 +0200

    Revert "Run purty"
    This reverts commit 3292def0089584e20174238d62e329c4cec9926d.

--- src/Main.purs
+++ src/Main.purs
@@ -17,6 +17,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 -}
+
 module Main where
 
 import Prelude
@@ -24,24 +25,23 @@ import Prelude
 import Effect (Effect)
 import Effect.Aff (forkAff)
 import Effect.Class (liftEffect)
+import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 import Intl (localiseString)
 import Intl.Locales (preferredUserLanguages)
-
-import AppRouting.Router as Router
-import Halogen.Aff as HA
 import Model as ML
+import AppRouting.Router as Router
 
 foreign import removeLoader :: Effect Unit
 
 main :: Effect Unit
 main = HA.runHalogenAff do
-    userLanguages <- liftEffect preferredUserLanguages
-    let translate = localiseString userLanguages
-        
-    let initialModel = ML.initial translate
-        
-    body <- HA.awaitBody
-    _ <- liftEffect removeLoader
-    router <- runUI (Router.component initialModel) unit body
-    forkAff $ Router.routeSignal router
+  userLanguages <- liftEffect preferredUserLanguages
+  let translate = localiseString userLanguages
+  let initialModel = ML.initial translate
+  body <- HA.awaitBody
+  _ <- liftEffect removeLoader
+  router <- runUI (Router.component initialModel) unit body
+  forkAff $ Router.routeSignal router
+
+
