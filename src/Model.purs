6a9a3b006d05109c8932c42be05091f5b9800beb
Alexandra DeWit <alexa.dewit@gmail.com>
2018-07-14 15:55:34 +0200

    Create session concept on model

--- src/Model.purs
+++ src/Model.purs
@@ -1,16 +1,29 @@
 module Model where
 
-import Prelude (show)
-import Intl.Terms (Term)
 import AppRouting.Routes as Routes
+import Crypt.NaCl.Types (BoxKeyPair, SecretBoxKey)
+import Data.Maybe (Maybe(..))
+import Intl.Terms (Term)
+
+newtype KeyChain = KeyChain
+  { secretBoxKey :: SecretBoxKey
+  , boxKeyPair :: BoxKeyPair
+  }
+
+type Session =
+  { userName :: String
+  , keyChains :: KeyChain
+  }
 
 type Model =
   { localiseFn :: Term -> String
   , currentPage :: Routes.Routes
+  , session :: Maybe Session
   }
 
 initial :: (Term -> String) -> Model
 initial f =
   { localiseFn: f
   , currentPage: Routes.Resources
+  , session: Nothing
   }
