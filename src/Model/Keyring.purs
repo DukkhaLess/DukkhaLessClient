df9fac6cb03c812b5284e105765e84c3fd5b0a2e
Alexandra DeWit <alexa.dewit@gmail.com>
2018-07-21 21:03:57 +0200

    Finish encoder

--- src/Model/Keyring.purs
+++ src/Model/Keyring.purs
@@ -1,12 +1,18 @@
-module Model.Keyring where
+module Model.Keyring
+  ( generateKeyring
+  , runSecretBoxKey
+  , runBoxKeyPair
+  , encodeKey
+  , Keyring(..)
+  ) where
 
-import Crypt.NaCl (SecretBoxKey, generateBoxKeyPair, generateSecretBoxKey, toUint8Array)
+import Crypt.NaCl (BoxKeyPair(..), SecretBoxKey, generateBoxKeyPair, generateSecretBoxKey, toUint8Array)
 import Crypt.NaCl.Types (BoxKeyPair, SecretBoxKey)
 import Data.Argonaut.Core (Json, caseJsonObject, jsonEmptyObject)
 import Data.Argonaut.Decode (class DecodeJson)
 import Data.Argonaut.Decode.Combinators ((.?))
 import Data.Argonaut.Encode (class EncodeJson)
-import Data.Argonaut.Encode.Combinators ((~>), assoc)
+import Data.Argonaut.Encode.Combinators ((~>), (:=))
 import Data.ArrayBuffer.DataView (whole, buffer)
 import Data.ArrayBuffer.Typed (asUint8Array, dataView)
 import Data.ArrayBuffer.Types (Uint8Array)
@@ -33,10 +39,20 @@ newtype Keyring = Keyring
 runSecretBoxKey :: Keyring -> SecretBoxKey
 runSecretBoxKey (Keyring keyring) = keyring.secretBoxKey
 
+runBoxKeyPair :: Keyring -> BoxKeyPair
+runBoxKeyPair (Keyring keyring) = keyring.boxKeyPair
+
 instance encodeKeyringJson :: EncodeJson Keyring where
   encodeJson ring
-    = assoc "secretBoxKey" (encodeKey $ toUint8Array $ runSecretBoxKey ring)
+    = "secretBoxKey" := (encodeKey $ toUint8Array $ runSecretBoxKey ring)
+    ~> "boxKeyPair" := (encodeBoxKeyPair $ runBoxKeyPair ring)
     ~> jsonEmptyObject
+
+encodeBoxKeyPair :: BoxKeyPair -> Json
+encodeBoxKeyPair (BoxKeyPair pair)
+  = "publicKey" := (encodeKey $ toUint8Array pair.publicKey)
+  ~> "secretKey" := (encodeKey $ toUint8Array pair.secretKey)
+  ~> jsonEmptyObject
 
 instance decodeKeyringJson :: DecodeJson Keyring where
   decodeJson json =
