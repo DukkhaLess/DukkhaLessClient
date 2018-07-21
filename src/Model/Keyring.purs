5ec0b64e1ebb0f04745fb6c338003230c88aa1b0
Alexandra DeWit <alexa.dewit@gmail.com>
2018-07-21 20:24:19 +0200

    Work in progress on implementing the codecs for the keys

--- src/Model/Keyring.purs
+++ src/Model/Keyring.purs
@@ -1,9 +1,20 @@
 module Model.Keyring where
 
-import Prelude    (pure, bind, ($))
-import Effect     (Effect)
-import Crypt.NaCl (generateBoxKeyPair, generateSecretBoxKey)
+import Crypt.NaCl (generateBoxKeyPair, generateSecretBoxKey, toUint8Array)
 import Crypt.NaCl.Types (BoxKeyPair, SecretBoxKey)
+import Data.Argonaut.Core (Json, caseJsonObject, jsonEmptyObject)
+import Data.Argonaut.Decode (class DecodeJson)
+import Data.Argonaut.Decode.Combinators ((.?))
+import Data.Argonaut.Encode (class EncodeJson)
+import Data.Argonaut.Encode.Combinators ((~>), assoc)
+import Data.ArrayBuffer.DataView (whole, buffer)
+import Data.ArrayBuffer.Typed (asUint8Array, dataView)
+import Data.ArrayBuffer.Types (Uint8Array)
+import Data.Base64 (encodeBase64, decodeBase64, runBase64)
+import Data.Either (Either(..))
+import Effect (Effect)
+import Foreign.Object (Object)
+import Prelude (pure, bind, ($), (<<<))
 
 generateKeyring :: Effect Keyring
 generateKeyring = do
@@ -11,8 +22,27 @@ generateKeyring = do
   secretBoxKey <- generateSecretBoxKey
   pure $ Keyring { secretBoxKey: secretBoxKey, boxKeyPair: boxKeyPair }
 
+encodeKey :: Uint8Array -> String
+encodeKey = runBase64 <<< encodeBase64 <<< buffer <<< dataView
+
 newtype Keyring = Keyring
   { secretBoxKey :: SecretBoxKey
   , boxKeyPair :: BoxKeyPair
   }
 
+runSecretBoxKey :: Keyring -> Uint8Array
+runSecretBoxKey (Keyring keyring) = toUint8Array $ keyring.secretBoxKey
+
+instance encodeKeyringJson :: EncodeJson Keyring where
+  encodeJson ring
+    = assoc "secretBoxKey" (encodeKey $ runSecretBoxKey ring)
+    ~> jsonEmptyObject
+
+instance decodeKeyringJson :: DecodeJson Keyring where
+  decodeJson json =
+    caseJsonObject (Left "Keyring input was not a JSON Object") parseRingObj json where
+    parseRingObj :: Object Json -> Either String Keyring
+    parseRingObj ringObj = do
+      sbk <- ringObj .? "secretBoxKey"
+      bkp <- ringObj .? "boxKeyPair"
+      pure $ Keyring { secretBoxKey: sbk, boxKeyPair: bkp }
