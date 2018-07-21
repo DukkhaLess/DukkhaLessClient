61258dec77f27ada7e916fdc854f3c1163c2ca95
Alexandra DeWit <alexa.dewit@gmail.com>
2018-07-21 18:03:45 +0200

    Generate keyrings

--- src/Model/Keyring.purs
+++ src/Model/Keyring.purs
@@ -1,6 +1,15 @@
 module Model.Keyring where
 
+import Prelude    (pure, bind, ($))
+import Effect     (Effect)
+import Crypt.NaCl (generateBoxKeyPair, generateSecretBoxKey)
 import Crypt.NaCl.Types (BoxKeyPair, SecretBoxKey)
+
+generateKeyring :: Effect Keyring
+generateKeyring = do
+  boxKeyPair <- generateBoxKeyPair
+  secretBoxKey <- generateSecretBoxKey
+  pure $ Keyring { secretBoxKey: secretBoxKey, boxKeyPair: boxKeyPair }
 
 newtype Keyring = Keyring
   { secretBoxKey :: SecretBoxKey
