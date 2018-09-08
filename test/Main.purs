e4ddd0f1fb5a9a3c9381c409ca36aac147a2fe63
Alexandra DeWit <alexa.dewit@gmail.com>
2018-09-08 19:51:59 +0200

    Test passing json round trip wow!

--- test/Main.purs
+++ test/Main.purs
@@ -4,8 +4,9 @@ import Prelude
 import Effect         (Effect)
 import Effect.Console (log)
 import Test.Model.Keyring as KR
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (run)
 
 main :: Effect Unit
-main = do
-  log "You should add some tests."
+main = run [consoleReporter] do
   KR.test
--- test/Model/Keyring.purs
+++ test/Model/Keyring.purs
@@ -1,11 +1,26 @@
 module Test.Model.Keyring where
 
-import Prelude        (Unit)
-import Effect         (Effect)
+import Effect (Effect)
+import Effect.Class (liftEffect)
 import Effect.Console (log)
+import Data.Either (Either(..))
+import Model.Keyring (generateKeyring)
+import Prelude
 import Test.QuickCheck (quickCheck', (<?>), quickCheck)
 import Test.QuickCheck.Arbitrary (class Arbitrary, arbitrary)
+import Test.Spec (Spec, it, describe)
+import Test.Spec.Assertions (fail)
+import Data.Argonaut.Decode (decodeJson)
+import Data.Argonaut.Encode (encodeJson)
 
-test :: Effect Unit
+test :: Spec Unit
 test = do
-  log "Keyring tests"
+  describe "Keyring" do
+    it "Performs a JSON encoding round-trip" do
+      keyring <- liftEffect $ generateKeyring
+      let json = encodeJson $ keyring
+      let decoded = decodeJson $ json
+      case decoded of
+        Right result -> if result /= keyring then fail "Round trip failed. Result differed from original." else pure unit
+        Left failureReason -> fail $ "Failed to decode the generated json during round trip, error given was:\n" <> failureReason
+
