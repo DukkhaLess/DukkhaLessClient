cc66e445a121f12af04130c034f8302fb9122ad2
Alexandra DeWit <alexa.dewit@gmail.com>
2018-09-22 08:34:46 +0200

    Fix tag

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
-docker build -t $TRAVIS_COMMIT -t MySelfcareClient .
+docker build -t $TRAVIS_COMMIT -t myselfcareclient .
 docker push $DOCKER_REPOSITORY
