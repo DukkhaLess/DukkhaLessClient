9112b4c1fbd101e71b2457b5e9af4aed968af87d
Alexandra DeWit <alexa.dewit@gmail.com>
2018-09-22 08:40:21 +0200

    Fix name for docker tag to match

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
-docker build -t $TRAVIS_COMMIT -t myselfcareclient .
+docker build -t $TRAVIS_COMMIT -t $DOCKER_REPOSITORY .
 docker push $DOCKER_REPOSITORY
