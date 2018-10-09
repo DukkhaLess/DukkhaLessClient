1cec420031a73b2ff477448ccb902875618ca309
Alexandra DeWit <alexa.dewit@gmail.com>
2018-10-09 03:51:56 +0200

    Improve form helper

--- src/Components/Helpers/Forms.purs
+++ src/Components/Helpers/Forms.purs
@@ -9,23 +9,37 @@ import Data.Semigroup ((<>))
 import Halogen.HTML as HH
 import Halogen.HTML.Core (HTML)
 import Halogen.HTML.Properties (IProp)
+import Halogen.HTML.Properties as HP
 import Intl (LocaliseFn)
 import Intl.Terms (Term)
 import Prelude (($), const, map)
+import Style.Bulogen as SB
 import Unsafe.Coerce (unsafeCoerce)
 
 validated' :: forall a r p i. Validation a r -> LocaliseFn -> HTML p i -> HTML p i
-validated' v t f = validated v t (map unwrap) f []
+validated' v t f =
+  validated
+    v
+    t
+    (map unwrap)
+    f
+    [ HP.classes
+      [ SB.danger
+      , SB.help
+      ]
+    ]
 
 validated :: forall a e r r2 p i. ValidationG e a r -> LocaliseFn -> (e -> Array Term) -> HTML p i -> Array (IProp r2 i) -> HTML p i
 validated v t describeError field ps = elem where
   elem = HH.div
-    [
+    [ HP.classes
+      [ SB.control
+      ]
     ]
     children
   children = case inputState v of
     Dirty -> either (\es -> [field] <> errElems es)  (const [field]) result
     Initial -> [field]
-  errElem err = HH.div (unsafeCoerce ps) [ HH.text $ t err ]
+  errElem err = HH.p (unsafeCoerce ps) [ HH.text $ t err ]
   errElems = map errElem
   result = lmap describeError (validate v)
