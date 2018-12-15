5e26fb1f82a0ed36cb4567cd7652485847792e8d
Alexandra DeWit <alexa.dewit@gmail.com>
2018-12-15 23:25:01 +0100

    Provide encrypt instance

--- src/Data/Crypto/Class.purs
+++ src/Data/Crypto/Class.purs
@@ -2,18 +2,18 @@ module Data.Crypto.Class where
 
 import Prelude
 
-import Crypt.NaCl (boxAfter, boxBefore, boxOpenAfter, fromUint8Array, generateNonce, secretBox, secretBoxOpen, toUint8Array)
+import Crypt.NaCl (BoxPublicKey, BoxSecretKey, boxAfter, boxBefore, boxOpenAfter, fromUint8Array, generateNonce, secretBox, secretBoxOpen, toUint8Array)
 import Crypt.NaCl.Types (BoxSharedKey, Message, Nonce, SecretBoxKey)
 import Data.Argonaut.Decode (class DecodeJson)
 import Data.Argonaut.Encode (class EncodeJson)
 import Data.ArrayBuffer.ArrayBuffer (fromString)
-import Data.Base64 (Base64(..))
+import Data.Base64 (Base64(..), encodeBase64)
 import Data.Crypto.Types (DocumentMetaData, Document, EncryptedMessage(..), MessageContents(..), SenderPublicKey(..))
 import Data.Either (Either)
+import Data.Newtype (unwrap)
 import Data.TextEncoder (encodeUtf8)
 import Effect (Effect)
-import Model.Keyring (Keyring(..), boxPrivateKey)
-import Data.Newtype (unwrap)
+import Model.Keyring (Keyring(..), boxPrivateKey, boxPublicKey)
 
 class EncodeJson a <= CipherText a
 
@@ -24,7 +24,7 @@ data DecryptionError
   = Description String
 
 data CryptoKey
-  = BoxShared BoxSharedKey
+  = BoxPair BoxPublicKey BoxSecretKey
   | SecretBox SecretBoxKey
 
 class FindKey a where
@@ -33,30 +33,35 @@ class FindKey a where
 instance findkeyEncryptedMessage :: FindKey EncryptedMessage where
   findKey (EncryptedMessage message) (Keyring keyring) = 
     case message.contents of
-      (Boxed _ (SenderPublicKey senderPublicKey)) -> BoxShared (boxBefore senderPublicKey (boxPrivateKey $ Keyring keyring))
+      (Boxed _ (SenderPublicKey senderPublicKey)) -> BoxPair senderPublicKey (boxPrivateKey $ Keyring keyring)
       (SecretBoxed _) -> SecretBox keyring.secretBoxKey
 
 
 class (CipherText b, FindKey b, EncodeJson a, DecodeJson a) <= Encrypt a b where
-  encrypt :: a -> CryptoKey -> Effect b
+  encrypt :: a -> (Keyring -> CryptoKey) -> Keyring -> Effect b
   decrypt :: b -> Keyring -> Either DecryptionError a
 
 instance encryptStringMessage :: Encrypt Base64 EncryptedMessage where
-  encrypt (Base64 s) key = do
+  encrypt (Base64 s) keyFn keyring = do
     message <- fromUint8Array $ toUint8Array $ fromString s
     nonce <- generateNonce
-    let cryptFn = case key of
-          (BoxShared sharedKey) -> (\a b -> boxAfter a b sharedKey)
-          (SecretBox secretKey) -> (\a b -> secretBox a b secretKey)
-    pure $ cryptFn message nonce
+    pure $ encryptMessage message keyFn keyring nonce
   decrypt message ring = do
     let unwrappedMessage = unwrap message
-    let key = findKey message
+    let key = findKey message ring
     let nonce = unwrappedMessage.nonce
     let decryptFn = case key of
-          (BoxShared sharedKey) -> (\b n -> boxOpenAfter b n sharedKey)
+          (BoxPair senderPublic recipientPrivate) -> (\b n -> boxOpenAfter b n (boxBefore senderPublic recipientPrivate)
           (SecretBox secretKey) -> (\b n -> secretBoxOpen b n secretKey)
     pure $ decryptFn message nonce
 
-
-    
+encryptMessage :: Message -> (Keyring -> CryptoKey) -> Keyring -> Nonce -> EncryptedMessage
+encryptMessage message keyFn keyring nonce = cryptoFn key where
+  key :: CryptoKey
+  key = keyFn keyring 
+  cryptoFn :: CryptoKey -> EncryptedMessage
+  cryptoFn (BoxPair recipientPublic senderPrivate) = EncryptedMessage { nonce: nonce, contents: contents } where
+    contents = Boxed (boxAfter message nonce sharedKey) (SenderPublicKey $ boxPublicKey keyring)
+    sharedKey = boxBefore recipientPublic senderPrivate
+  cryptoFn (SecretBox secretKey) =  EncryptedMessage { nonce: nonce, contents: contents } where
+    contents =  SecretBoxed (secretBox message nonce secretKey)
