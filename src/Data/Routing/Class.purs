4df4e9c9f3552ea6e6a924048e144f613c3fdc1a
Alexandra DeWit <alexa.dewit@gmail.com>
2019-01-30 13:51:49 +0100

    Delete dead code

--- src/Data/Routing/Class.purs
+++ src/Data/Routing/Class.purs
@@ -1,4 +1,10 @@
 module Data.Routing.Class where
 
+import Intl.Terms (Term)
+
 class ReverseRoute a where
   reverseRoute :: a -> String
+
+class DescribeRoute a where
+  describe :: a -> Term
+  iconize :: a -> String
