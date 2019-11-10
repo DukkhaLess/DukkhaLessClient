ffc9f98980f2485ffd7eac8ab0f35ae04c667823
Alexandra DeWit <alexa.dewit@gmail.com>
2019-11-10 17:33:33 -0500

    Fix several errors

--- src/Data/HTTP/Request.purs
+++ src/Data/HTTP/Request.purs
@@ -2,7 +2,6 @@ module Data.HTTP.Request where
 
 import Data.HTTP.Helpers
 import Prelude
-
 import Affjax (Response)
 import Data.Bifunctor (lmap)
 import Data.Crypto.Class (decrypt)
@@ -14,14 +13,13 @@ import Model (SessionToken)
 import Model.Journal (JournalEntry(..))
 import Model.Keyring (Keyring(..))
 
-getJournalEntry
-  :: forall m
-  . MonadAff m
-  => SessionToken
-  -> DocumentId
-  -> Keyring
-  -> m (Either DecryptionError JournalEntry)
+getJournalEntry ::
+  forall m.
+  MonadAff m =>
+  SessionToken ->
+  DocumentId ->
+  Keyring ->
+  m (Either DecryptionError JournalEntry)
 getJournalEntry sessionToken id keyring = do
   response <- request (get (ApiPath ("/journals/" <> unwrap id)) sessionToken) :: m (Response (Either String Document))
   pure $ (lmap Description response.body) >>= decrypt keyring
-
