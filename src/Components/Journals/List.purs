f30752c01668840becb884f5ada891264fe27c34
Alexandra DeWit <alexa.dewit@gmail.com>
2019-11-12 16:44:41 -0500

    Fix further build failures

--- src/Components/Journals/List.purs
+++ src/Components/Journals/List.purs
@@ -1,57 +1,43 @@
 module Components.Journals.List where
 
 import Prelude
-
 import AppM (CurrentSessionRow, JournalMetaCacheRow)
 import Control.Monad.Reader.Class (class MonadAsk)
-import Data.Crypto.Types (DocumentId)
-import Data.Map (Map)
-import Data.Maybe (Maybe(..))
+import Data.Const (Const)
 import Effect.Aff.Class (class MonadAff)
+import Halogen (mkEval)
 import Halogen as H
 import Halogen.HTML as HH
-import Halogen.HTML.Events as HE
-import Halogen.HTML.Properties as HP
 import Intl (LocaliseFn)
-import Model (Session(..))
-import Model.Journal (JournalMeta(..))
 import Type.Row (type (+))
 
-data Query a = NoOp a
+newtype State
+  = State {}
 
-data Slot = Slot
-derive instance eqSlot :: Eq Slot
-derive instance ordSlot :: Ord Slot
-
-data Message = MNoMsg
-
-newtype State = State {}
+type ChildSlots
+  = ()
 
 initialState :: forall a. a -> State
 initialState = const $ State {}
 
-type RequiredState r =
-  ( CurrentSessionRow
-  + JournalMetaCacheRow
-  + r
-  )
-component
-  :: forall a m r
-  . MonadAff m
-  => MonadAsk (Record (RequiredState r)) m
-  => LocaliseFn
-  -> H.Component HH.HTML Query a Message m
+type RequiredState r
+  = ( CurrentSessionRow
+        + JournalMetaCacheRow
+        + r
+    )
+
+component ::
+  forall a m r o.
+  MonadAff m =>
+  MonadAsk (Record (RequiredState r)) m =>
+  LocaliseFn ->
+  H.Component HH.HTML (Const Unit) a o m
 component t =
-  H.component
+  H.mkComponent
     { initialState: initialState
     , render
-    , eval
-    , receiver: const Nothing
+    , eval: mkEval $ H.defaultEval
     }
   where
-
-  render :: State -> H.ComponentHTML Query
+  render :: State -> H.ComponentHTML a ChildSlots m
   render _ = HH.text "Hi"
-
-  eval :: Query ~> H.ComponentDSL State Query Message m
-  eval (NoOp next) = pure next
