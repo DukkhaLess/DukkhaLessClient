208028d3c988ecf025be5eb879734dcc5de37188
Alexandra DeWit <alexa.dewit@gmail.com>
2019-11-16 21:46:01 -0500

    Starting on cleanup of Login

--- src/Components/Sessions/Login.purs
+++ src/Components/Sessions/Login.purs
@@ -1,7 +1,6 @@
 module Components.Sessions.Login where
 
 import Prelude
-
 import Components.Helpers.Forms as HF
 import Control.Monad.Error.Class (throwError)
 import Control.Monad.State (class MonadState, get, put, modify_)
@@ -32,38 +31,42 @@ import Web.HTML.Event.DataTransfer (files)
 import Web.HTML.Event.DataTransfer as HA
 import Web.HTML.Event.DragEvent (dataTransfer)
 
-data Query a
-  = UpdateKey String a
-  | UpdateUsername String a
-  | UpdatePassword String a
-  | DropFiles (Maybe FileList) a
-  | Submit a
+data Action
+  = UpdateKey String
+  | UpdateUsername String
+  | UpdatePassword String
+  | DropFiles (Maybe FileList)
+  | Submit
 
 data Message
   = SessionCreated Session
 
-type State =
-  { username :: Username
-  , password :: Password
-  , keyring :: V.Validation String Keyring
-  }
+type State
+  = { username :: Username
+    , password :: Password
+    , keyring :: V.Validation String Keyring
+    }
 
 initialState :: forall a. a -> State
-initialState = const
-                 { username: (wrap "")
-                 , password: (wrap "")
-                 , keyring: V.validation (VR.parsableKeyring) ""
-                 }
+initialState =
+  const
+    { username: (wrap "")
+    , password: (wrap "")
+    , keyring: V.validation (VR.parsableKeyring) ""
+    }
 
-data Slot = Slot
+data Slot
+  = Slot
+
 derive instance eqSlot :: Eq Slot
+
 derive instance ordSlot :: Ord Slot
 
-component
-  :: forall a m
-  . MonadAff m
-  => LocaliseFn
-  -> H.Component HH.HTML Query a Message m
+component ::
+  forall m.
+  MonadAff m =>
+  LocaliseFn ->
+  H.Component HH.HTML (Const Void) Void Message m
 component t =
   H.component
     { initialState: initialState
@@ -72,61 +75,60 @@ component t =
     , receiver: const Nothing
     }
   where
-
   render :: State -> H.ComponentHTML Query
   render state =
-          HH.section [HP.classes [hero]]
-              [ HH.div [HP.classes [heroBody]]
-                [ HH.div [HP.classes [container]]
-                  [ HH.h1 [ HP.classes [title]] [ HH.text $ t $ Term.Session Sessions.Login ]
-                  , HH.input
-                    [ HP.classes [input]
-                    , HP.placeholder $ t $ Term.Session Sessions.Username
-                    , HE.onValueChange (HE.input UpdateUsername)
-                    ]
-                  , HH.input
-                    [ HP.type_ HP.InputPassword
-                    , HP.classes [input]
-                    , HP.placeholder $ t (Term.Session Sessions.Password)
-                    , HE.onValueChange (HE.input UpdatePassword)
-                    ]
-                  , HF.validated' state.keyring t $ HH.textarea
-                        [ HE.onValueChange (HE.input UpdateKey)
-                        , HP.classes [textarea]
-                        , HP.placeholder "Your secret keys."
-                        , HE.onDrop $ HE.input (dataTransfer >>> files >>> DropFiles)
-                        ]
-                  , HH.a
-                    [ HP.classes [button, success, block]
-                    , HE.onClick (HE.input_ Submit)
-                    ]
-                    [ HH.text $ t $ Term.Session Sessions.Submit
-                    ]
-                  , HH.a
-                    [ HP.classes [block, textCentered]
-                    , HP.href $ R.reverseRoute $ R.Sessions RS.Register
-                    ]
-                    [ HH.text $ t $ Term.Session Sessions.RegisterInstead
-                    ]
+    HH.section [ HP.classes [ hero ] ]
+      [ HH.div [ HP.classes [ heroBody ] ]
+          [ HH.div [ HP.classes [ container ] ]
+              [ HH.h1 [ HP.classes [ title ] ] [ HH.text $ t $ Term.Session Sessions.Login ]
+              , HH.input
+                  [ HP.classes [ input ]
+                  , HP.placeholder $ t $ Term.Session Sessions.Username
+                  , HE.onValueChange (HE.input UpdateUsername)
                   ]
-                ]
+              , HH.input
+                  [ HP.type_ HP.InputPassword
+                  , HP.classes [ input ]
+                  , HP.placeholder $ t (Term.Session Sessions.Password)
+                  , HE.onValueChange (HE.input UpdatePassword)
+                  ]
+              , HF.validated' state.keyring t
+                  $ HH.textarea
+                      [ HE.onValueChange (HE.input UpdateKey)
+                      , HP.classes [ textarea ]
+                      , HP.placeholder "Your secret keys."
+                      , HE.onDrop $ HE.input (dataTransfer >>> files >>> DropFiles)
+                      ]
+              , HH.a
+                  [ HP.classes [ button, success, block ]
+                  , HE.onClick (HE.input_ Submit)
+                  ]
+                  [ HH.text $ t $ Term.Session Sessions.Submit
+                  ]
+              , HH.a
+                  [ HP.classes [ block, textCentered ]
+                  , HP.href $ R.reverseRoute $ R.Sessions RS.Register
+                  ]
+                  [ HH.text $ t $ Term.Session Sessions.RegisterInstead
+                  ]
               ]
+          ]
+      ]
 
   secretKeyHeader :: forall p i. HH.HTML p i
   secretKeyHeader =
     HH.h4
-      [ HP.classes [subtitle]
+      [ HP.classes [ subtitle ]
       ]
       [ HH.text $ t $ Term.Session Sessions.KeySubtitle
       ]
 
-
-  eval
-    :: forall t
-    . MonadState State t
-    => MonadAff t
-    => (Message -> t Unit)
-    -> Query ~> t
+  eval ::
+    forall t.
+    MonadState State t =>
+    MonadAff t =>
+    (Message -> t Unit) ->
+    Query ~> t
   eval raise query = case query of
     (UpdateUsername username next) -> do
       state <- get
@@ -138,15 +140,18 @@ component t =
       pure next
     (UpdateKey keyStr next) -> do
       state <- get
-      let keyringValidation = V.updateValidation state.keyring keyStr
+      let
+        keyringValidation = V.updateValidation state.keyring keyStr
       put state { keyring = keyringValidation }
       pure next
     (Submit next) -> do
       state <- get
-      modify_ (_{ keyring = V.touch state.keyring })
+      modify_ (_ { keyring = V.touch state.keyring })
       payloadAndKeyring <- liftAff $ either throwError pure (prepareLoginPayload state)
-      let payload = fst payloadAndKeyring
-      let keyring = snd payloadAndKeyring
+      let
+        payload = fst payloadAndKeyring
+      let
+        keyring = snd payloadAndKeyring
       response <- liftAff $ request (plaintextPost (ApiPath "/login") payload)
       case response.body of
         Right token -> do
@@ -158,10 +163,12 @@ component t =
       pure next
 
   prepareLoginPayload :: State -> Either Error (Tuple SubmitLogin Keyring)
-  prepareLoginPayload state = note (error "Validation failed for login payload") result where
+  prepareLoginPayload state = note (error "Validation failed for login payload") result
+    where
     keyring :: Maybe Keyring
     keyring = V.validate_ state.keyring
+
     payload :: SubmitLogin
     payload = SubmitLogin { username: state.username, password: state.password }
+
     result = map (Tuple payload) keyring
-    
